45eaeb01342ed01689cfc3fe6e069d4b11e723f1
JulioRueda <jc.ruedah@up.edu.pe>
2025-01-15 23:25:20 -0500

    last line main change

--- server/main.py
+++ server/main.py
@@ -77,4 +77,5 @@ def plot_time():
     return jsonify(y.tolist())
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 8081)
+    port = int(os.environ.get("PORT", 8081))
+    app.run(debug=True, port=port)
